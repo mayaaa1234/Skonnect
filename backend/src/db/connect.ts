d4321ac8728f92cd22384ef77c8f4ec88d09027e
isMaya404 <franciajeremiah10@gmail.com>
2025-05-21 12:52:44 +0800

    .

--- backend/src/db/connect.ts
+++ backend/src/db/connect.ts
@@ -2,11 +2,13 @@ import mysql from "mysql2/promise";
 // import dotenv from "dotenv";
 // dotenv.config();
 
-//console.log("NODE_ENV:", process.env.NODE_ENV);
-//console.log("MYSQLHOST:", process.env.MYSQLHOST);
-//console.log("MYSQLUSER:", process.env.MYSQLUSER);
-//console.log("MYSQLDATABASE:", process.env.MYSQL_DATABASE);
-//console.log("MYSQLPORT:", process.env.MYSQLPORT);
+// console.log("NODE_ENV:", process.env.NODE_ENV);
+// console.log("MYSQLHOST:", process.env.MYSQLHOST);
+// console.log("MYSQLUSER:", process.env.MYSQLUSER);
+// console.log("MYSQLDATABASE:", process.env.MYSQL_DATABASE);
+// console.log("MYSQLPORT:", process.env.MYSQLPORT);
+
+console.log("MYSQLPORT:", process.env.MYSQL_PUBLIC_URL);
 
 const connectDB = () => {
   if (process.env.NODE_ENV === "development") {
@@ -18,7 +20,6 @@ const connectDB = () => {
       multipleStatements: true,
     });
   }
-
   else {
     return mysql.createConnection(process.env.MYSQL_PUBLIC_URL as string);
   }

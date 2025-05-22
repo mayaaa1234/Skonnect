6dec7e8a2ce380db8e00a5f3b3191c7d27c796b3
isMaya404 <franciajeremiah10@gmail.com>
2025-05-22 09:44:27 +0800

    fixed sidebar toggle z index issue on sm screens

--- frontend/src/ts/pages/profile/sidebar/toggle.ts
+++ frontend/src/ts/pages/profile/sidebar/toggle.ts
@@ -1,6 +1,6 @@
 const showSidebarBtn = document.querySelector(
   ".profile-btn-show-sidebar",
-) as HTMLElement;
+)
 const hideSidebarBtn = document.querySelector(
   ".sidebar-btn-hide-sidebar",
 ) as HTMLElement;
@@ -9,7 +9,28 @@ const dataPage = document.documentElement.dataset.page;
 const dom = document.documentElement;
 
 // TOGGLING SIDEBAR
-//
+
+if (dataPage === "profile" && sessionStorage.getItem("active-sidebar")) {
+  dom.classList.toggle("active-sidebar", true);
+}
+
+showSidebarBtn!.addEventListener("click", () => {
+  console.log("clicked sidebar toggle");
+  if (dataPage === "profile") {
+    dom.classList.add("active-sidebar");
+    sessionStorage.setItem("active-sidebar", "true");
+  }
+});
+
+hideSidebarBtn?.addEventListener("click", () => {
+  console.log("clicked sidebar toggle");
+  if (dataPage && dataPage === "profile") {
+    dom.classList.remove("active-sidebar");
+    sessionStorage.removeItem("active-sidebar");
+  }
+});
+
+// Clear active sidebar on page change
 export const clearActiveSidebar = () => {
   const currentDataPage = document.documentElement.dataset.page;
 
@@ -17,54 +38,6 @@ export const clearActiveSidebar = () => {
     sessionStorage.removeItem("active-sidebar");
   }
 };
-
-if (dataPage === "profile" && sessionStorage.getItem("active-sidebar")) {
-  dom.classList.toggle("active-sidebar", true);
-}
-
-showSidebarBtn.addEventListener("click", () => {
-  console.log("clicked sidebar toggle");
-  if (dataPage && dataPage === "profile") {
-    dom.classList.toggle("active-sidebar");
-    sessionStorage.setItem("active-sidebar", "true");
-  }
-});
-
-// <% if (user.isAdmin) { %>
-// <button class="btn-dark-accent p-1 br-40" id="upload-trigger-btn">
-//     Add Slideshow
-// </button>
-//
-// <div class="upload-popup-overlay" id="upload-popup-overlay">
-//     <div class="upload-popup">
-//         <div class="popup-header">
-//             <h2>Upload Images</h2>
-//             <p>
-//                 NOTE: <br />
-//                 supported files: png, jpg, jpeg, webp<br />
-//                 recommended 6-8 images only for storage
-//                 considerations
-//             </p>
-//             <!-- <button id="close-upload-popup">&times;</button> -->
-//         </div>
-//
-//         <form id="upload-form">
-//             <div id="drop-zone">
-//                 <p>Drop the file here or click to upload</p>
-//                 <input
-//                     type="file"
-//                     id="image-upload"
-//                     accept=".jpg,.jpeg,.png,.webp"
-//                     multiple
-//                     required
-//                 />
-//                 <div class="img-preview-container"></div>
-//             </div>
-//             <button type="submit">Upload</button>
-//         </form>
-//     </div>
-// </div>
-// <% } %>
 
 // document.addEventListener("click", (e) => {
 //   if (!sidebar || !showSidebarBtn) return;
@@ -78,13 +51,4 @@ showSidebarBtn.addEventListener("click", () => {
 //     !showSidebarBtn.contains(t)
 //   )
 //     dom.classList.remove("active-sidebar");
-//   }
 // });
-
-hideSidebarBtn?.addEventListener("click", () => {
-  console.log("clicked sidebar toggle");
-  if (dataPage && dataPage === "profile") {
-    dom.classList.toggle("active-sidebar", false);
-    sessionStorage.removeItem("active-sidebar");
-  }
-});
